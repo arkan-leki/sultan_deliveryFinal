0c97441933d536b7c78e8f48201a4efec31fc67e
Arkan Laki <arkan.leki@gmail.com>
2020-07-02 21:02:29 +0300

    flutter

--- api/serializer.py
+++ api/serializer.py
@@ -32,59 +32,64 @@ class FoodSerializer(serializers.HyperlinkedModelSerializer):
         model = models.Food
         fields = ['id', 'url', 'category', 'category_id', 'category_war', 'image', 'sell_price', 'title', 'subtitle', 'detiles','date_add', 'deleted', 'cost', 'popularity' , 'avg_ratings']
 
-class SpecifySerializer(serializers.ModelSerializer):
+class SpecifySerializer(serializers.HyperlinkedModelSerializer):
+    food = serializers.HyperlinkedIdentityField(view_name="api:food-detail")
+    url = serializers.HyperlinkedIdentityField(view_name="api:specify-detail")
     class Meta:
         model = models.Specify
-        fields = ['id','food','title', 'price','date_added','food_id']
+        fields = ['id', 'url', 'food','title', 'price','date_added','food_id']
 
-class CustomerSerializer(serializers.ModelSerializer):
+class CustomerSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="api:customer-detail")
     class Meta:
         model = models.Customer
-        fields = ['id', 'name', 'password', 'instrument_purchase','house_no','address_line1', 'address_line2', 'phone', 'phoneid', 'zip_code', 'country', 'image']
+        fields = ['id', 'url', 'name', 'password', 'instrument_purchase','house_no','address_line1', 'address_line2', 'phone', 'phoneid', 'zip_code', 'country', 'image']
 
-class RateSerializer(serializers.ModelSerializer):
+class RateSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="api:rate-detail")
     class Meta:
         model = models.Rate
-        fields = ['id', 'food','customer', 'stars']
+        fields = ['id', 'url', 'food','customer', 'stars']
 
-class MotorSerializer(serializers.ModelSerializer):
+class MotorSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="api:motor-detail")
     class Meta:
         model = models.Motors
-        fields = ['id', 'title','number', 'image', 'status']
+        fields = ['id', 'url', 'title','number', 'image', 'status']
 
-class SpecifySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Specify
-        fields = ['id','food','title', 'price','date_added','food_id']
-
-
-class RequestSerializer(serializers.ModelSerializer):
+class RequestSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="api:request-detail")
+    customer = serializers.HyperlinkedIdentityField(view_name="api:customer-detail")
     request_detail = serializers.PrimaryKeyRelatedField(many=True, queryset= RequestDetail.objects.all())
     class Meta:
         model = models.Request
-        fields = ['id', 'name', 'phone', 'address','total_price', 'dashkandin', 'request_detail','customer', 'date_added','last_edit' , 'status']
+        fields = ['id', 'url', 'name', 'phone', 'address','total_price', 'dashkandin', 'request_detail','customer', 'date_added','last_edit' , 'status']
 
-class RequestDetailSerializer(serializers.ModelSerializer):
+class RequestDetailSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="api:requestDetail-detail")
+    customer = serializers.HyperlinkedIdentityField(view_name="api:customer-detail")
     food = serializers.CharField(source='food_id')
     food_title =  serializers.ReadOnlyField(source='food.title')
     specify = serializers.PrimaryKeyRelatedField(many=True, queryset= Specify.objects.all())
     class Meta:
         model = models.RequestDetail
-        fields = ['id','food', 'food_title', 'quantity', 'specify','total_price', 'dashkandin', 'customer','date_added','last_edit', 'status']
+        fields = ['id', 'url', 'food', 'food_title', 'quantity', 'specify','total_price', 'dashkandin', 'customer','date_added','last_edit', 'status']
 
-class DliverSerializer(serializers.ModelSerializer):
+class DliverSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="api:deliver-detail")
+    user = serializers.HyperlinkedIdentityField(view_name="api:account-detail")
     class Meta:
         model = models.Dliver
-        fields = ['id', 'name', 'user', 'motor', 'phone', 'phoneId', 'image']
+        fields = ['id', 'url', 'name', 'user', 'motor', 'phone', 'phoneId', 'image']
 
-class TransportSerializer(serializers.ModelSerializer):
+class TransportSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(view_name="api:transport-detail")
     dliver_name =  serializers.ReadOnlyField(source='dliver.name')
     request_name =  serializers.ReadOnlyField(source='request.name')
     request_total_price = serializers.ReadOnlyField(source='request.total_price')
     request_phoneid =  serializers.ReadOnlyField(source='request.phoneid')
     request_phone =  serializers.ReadOnlyField(source='request.phone')
     request_address =  serializers.ReadOnlyField(source='request.address')
-
     class Meta:
         model = models.Transport
-        fields = ['id', 'dliver', 'request', 'start_date', 'end_date', 'status','dliver_name', 'request_name','request_phoneid', 'request_phone','request_id','dliver_id','request_total_price','request_address']
+        fields = ['id', 'url', 'dliver', 'request', 'start_date', 'end_date', 'status','dliver_name', 'request_name','request_phoneid', 'request_phone','request_id','dliver_id','request_total_price','request_address']
