019a703071139a65c76a94a3de6be648da990631
Arkan Leki <arkan.leki@gmail.com>
2023-01-26 00:58:57 +0300

    master

--- sultan_delivery/settings.py
+++ sultan_delivery/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -194,8 +194,8 @@ else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-django_heroku.settings(locals())
-
+# django_heroku.settings(locals())
+# 
 SUIT_CONFIG = {
     'ADMIN_NAME': 'Django Suit',
     'MENU': (
