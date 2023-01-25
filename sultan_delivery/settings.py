90852e0b8c11cd035bae7c872e0c88ee54e046ae
Arkan Leki <arkan.leki@gmail.com>
2023-01-26 01:10:42 +0300

    db

--- sultan_delivery/settings.py
+++ sultan_delivery/settings.py
@@ -84,12 +84,24 @@ WSGI_APPLICATION = 'sultan_delivery.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '03rx5BYwT14Yi7Nbixgv',
+        'HOST': 'containers-us-west-162.railway.app',
+        'PORT': '6538',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
